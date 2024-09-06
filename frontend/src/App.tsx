d7678f2d44f686747f5b3d687bc7adf9530ac435
eraycancelik <cancelikeray@gmail.com>
2024-09-06 19:52:45 +0300

    design update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,21 +4,27 @@ import List from './components/List/List'
 import Navbar from './components/Navbar/Navbar'
 import { About, Contact, Home } from "./components/Pages"
 import Icon from './components/Navbar/Icon'
+import Footer from './components/Footer/Footer'
 const App = () => {
 
   return (
     <div className='bigwall'>
-      <Icon />
-      <Navbar />
-      <div className='wall'>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <h1>Internal API test</h1>
-        <List />
+      <div className='header'>
+        <Icon />
+        <Navbar />
       </div>
+      <div className='wall'>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <h1>Internal API test</h1>
+          <List />
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+
+import './Footer.css';
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="content">
+                <p>© 2024 Thermoprops</p>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
