cb48836423eaf519cdf3c350fd162229b0a60483
eraycancelik <cancelikeray@gmail.com>
2024-09-06 20:48:21 +0300

    frontend port changed

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:5173", "http://localhost:5173", "http://172.21.0.1:5173"]}})
+CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:80", "http://localhost:80", "http://172.21.0.1:80","http://localhost"]}})
 
 
 
