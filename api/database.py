8298d43a540a634043214fadb11c480cbf1c0d10
eraycancelik <cancelikeray@gmail.com>
2024-09-15 19:06:46 +0300

    docker file editted

---
+++ api/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import settings
+# Format of sqlalchemy database url:
+# "postgresql://<user_name>:<password>@<ip_address/hostname>:<database>"
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:159632159@postgres:5432/thermoprops_db"
+
+# Create a database engine
+# engine is used to connect to the database
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
