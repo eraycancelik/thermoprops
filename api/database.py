d6b10db547c9a15f9ff2f628ac0f62ca5a7bfc38
eraycancelik <cancelikeray@gmail.com>
2024-09-16 09:09:44 +0300

    docker-compose updated

--- api/database.py
+++ api/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 # Format of sqlalchemy database url:
 # "postgresql://<user_name>:<password>@<ip_address/hostname>:<database>"
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:dev-password@postgres:5432/thermoprops_db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:developmentPassword123456@postgres:5432/thermoprops_db"
 
 # Create a database engine
 # engine is used to connect to the database
@@ -15,7 +15,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-def get_db():
+def get_db(): 
     db = SessionLocal()
     try:
         yield db
